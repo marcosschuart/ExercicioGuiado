7bfed48be124dfcf618e01899780f8fb722f999a
marcosschuart <schuartmarcos@gmail.com>
2026-04-13 12:24:53 -0300

    Add files via upload

---
+++ App.java
@@ -0,0 +1,18 @@
+public class App {
+// 1. A Assinatura
+public static int maior(int a, int b) {
+
+    // 2. A Lógica Interna
+    if (a > b) {
+        return a;
+    } else {
+        return b;
+    }
+}
+
+    public static void main(String[] args) throws Exception {
+    // 3. O uso no main
+    int maximo = maior(10, 42);
+    System.out.println(maximo);
+    }
+}
